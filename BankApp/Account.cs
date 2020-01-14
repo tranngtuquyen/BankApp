c92dce12f43c064a5fae06eece2ca1e10541cdba
Quyen Tran <tranngtuquyen@gmail.com>
2020-01-13 16:47:01 -0800

    Initial Commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This represents a bank account 
+    /// where you could withdraw or deposit money into the account
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        private int lastAccountNumber = 0;
+        public int AccountNumber { get; private set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+        #region Constructor
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        /// <summary>
+        /// Withdraw money from account
+        /// </summary>
+        /// <param name="amount">Amount to withdraw</param>
+        /// <returns></returns>
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountNumber = 123456;
+            myAccount.Deposit(1000);
+            Console.WriteLine($"Account Number: {myAccount.AccountNumber}, Balance: {myAccount.Balance}");
+        }
+    }
+}
